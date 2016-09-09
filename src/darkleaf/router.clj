8b2a6991b5139b1ec42959e179204861dad7e174
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2016-09-09 09:27:17 +0400

    add low level api

--- src/darkleaf/router.clj
+++ src/darkleaf/router.clj
@@ -1,6 +1,95 @@
-(ns darkleaf.router)
+(ns darkleaf.router
+  (:require [clojure.core.match :refer [match]])
+  (:require [clojure.string :refer [split]]))
+(defrecord Route [name
+                  vars pattern
+                  handler scope])
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn route
+  [name & {:keys [vars pattern
+                  handler]
+           :or {vars #{}
+                pattern {}}}]
+  {:pre [(and
+          (keyword? name)
+          (set? vars)
+          (map? pattern)
+          (fn? handler))]}
+  (Route. name
+          vars pattern
+          handler '()))
+
+(defn- merge-segments-patterns [parent-segments child-segments]
+  (into parent-segments child-segments))
+
+(defn- merge-map-patterns [parent-map-pattern child-map-pattern]
+  (reduce-kv (fn [result k v]
+               (assoc result k
+                      (cond
+                        (and (map? (k result)) (map? v)) (merge-map-patterns (k result) v)
+                        (not (contains? result k)) v
+                        :else (throw (java.lang.IllegalArgumentException. "can't merge patterns: parent conrain child key")))))
+             parent-map-pattern
+             child-map-pattern))
+
+(defn- merge-request-patterns [parent child]
+  (let [parent-segments (get parent :segments [])
+        child-segments (get child :segments [])
+
+        parent-map-pattern (dissoc parent :segments)
+        child-map-pattern (dissoc child :segments)
+
+        result-segments (merge-segments-patterns parent-segments child-segments)
+        result-map-pattern (merge-map-patterns parent-map-pattern child-map-pattern)]
+    (assoc result-map-pattern :segments result-segments)))
+
+(defn scope [s-name
+             {:keys [vars pattern]
+              :or {vars #{}
+                   pattern {}}}
+             & routes]
+  {:pre [(and
+          (keyword? s-name)
+          (set? vars)
+          (map? pattern))]}
+  (map
+   (fn [route]
+     (-> route
+         (update :scope conj s-name)
+         (update :vars into vars)
+         (update :pattern #(merge-request-patterns pattern %))))
+   (flatten routes)))
+
+(defn- build-params-map [route]
+  (let [symbols (:vars route)
+        m-keys (vec (map keyword symbols))
+        m-vals (vec symbols)]
+    `(zipmap ~m-keys ~m-vals)))
+
+(defn- build-pattern-row [route]
+  [(:pattern route)])
+
+(defn- route->match-clause [route]
+  (let [pattern-row (build-pattern-row route)
+        action [route (build-params-map route)]]
+    (list pattern-row action)))
+
+(defmacro build-matcher [routes-var-name]
+  `(fn [req#]
+     (match
+      [req#]
+      ~@(let [routes (var-get (resolve routes-var-name))]
+          (mapcat route->match-clause routes)))))
+
+(defn- assoc-our-keys [req]
+  (assoc req :segments (vec (rest (split (:uri req) #"/")))))
+
+(defn build-handler [matcher]
+  (fn [-req]
+    (let [req (assoc-our-keys -req)
+          [route params] (matcher req)
+          route-handler (:handler route)
+          req-with-info (assoc req
+                               :route-params params
+                               :matched-route route)]
+      (route-handler req-with-info))))
--- test/darkleaf/router_test.clj
+++ test/darkleaf/router_test.clj
@@ -1,7 +1,60 @@
 (ns darkleaf.router-test
   (:require [clojure.test :refer :all]
-            [darkleaf.router :refer :all]))
+            [darkleaf.router :refer [route scope build-handler build-matcher]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def routes
+  (flatten
+   (list
+    (route :main-page
+           :pattern '{:segments [], :request-method :get}
+           :handler identity)
+    (route :legacy-page
+           :vars '#{slug}
+           :pattern '{:segments ["pages" (slug :guard #{"old-page"})], :request-method :get}
+           :handler identity)
+    (route :page
+           :vars '#{slug}
+           :pattern '{:segments ["pages" slug], :request-method :get}
+           :handler identity)
+    (scope :api
+           {:vars '#{api-token}
+            :pattern '{:segments ["api"], :headers {"token" api-token}}}
+           (route :create-page
+                  :pattern '{:segments ["pages"], :request-method :post}
+                  :handler identity)
+           (route :update-page
+                  :vars '#{slug}
+                  :pattern '{:segments ["pages" slug], :request-method :patch}
+                  :handler identity))
+    (route :not-found
+           :handler identity))))
+
+(deftest test-matcher
+  (let [matcher (build-matcher routes)]
+    (are [request r-name r-scope r-params] (= (let [[matched-route route-params] (matcher request)]
+                                                [(:name matched-route) (:scope matched-route) route-params])
+                                              [r-name r-scope r-params])
+      {:segments [], :request-method :get}
+      :main-page [] {}
+
+      {:segments ["pages" "about"], :request-method :get}
+      :page [] {:slug "about"}
+
+      {:segments ["pages" "old-page"], :request-method :get}
+      :legacy-page [] {:slug "old-page"}
+
+      {:segments ["api" "pages"], :request-method :post, :headers {"token" "secret"}}
+      :create-page [:api] {:api-token "secret"}
+
+      {:segments ["api" "pages" "contacts"], :request-method :patch, :headers {"token" "secret"}}
+      :update-page [:api] {:slug "contacts", :api-token "secret"})))
+
+(deftest test-handler
+  (let [matcher (build-matcher routes)
+        handler (build-handler matcher)
+        request {:uri "/pages/about", :request-method :get}
+        response (handler request)]
+    (is (= :page (get-in response [:matched-route :name])))
+    (is (= ["pages" "about"] (:segments response)))
+    (is (contains? response :matched-route))
+    (is (= {:slug "about"} (:route-params response)))))
