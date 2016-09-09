79ea02a8221e832dc1e8d44adb5f91e8897b0cd3
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2016-09-09 23:08:08 +0400

    refactoring

--- src/darkleaf/router.clj
+++ src/darkleaf/router.clj
@@ -1,6 +1,7 @@
 (ns darkleaf.router
   (:require [clojure.core.match :refer [match]])
   (:require [clojure.string :refer [split]]))
+
 (defrecord Route [name
                   vars pattern
                   handler scope])
@@ -19,28 +20,28 @@
           vars pattern
           handler '()))
 
-(defn- merge-segments-patterns [parent-segments child-segments]
+(defn- merge-segments-shapes [parent-segments child-segments]
   (into parent-segments child-segments))
 
-(defn- merge-map-patterns [parent-map-pattern child-map-pattern]
+(defn- merge-map-shapes [parent-map-pattern child-map-pattern]
   (reduce-kv (fn [result k v]
                (assoc result k
                       (cond
-                        (and (map? (k result)) (map? v)) (merge-map-patterns (k result) v)
+                        (and (map? (k result)) (map? v)) (merge-map-shapes (k result) v)
                         (not (contains? result k)) v
                         :else (throw (java.lang.IllegalArgumentException. "can't merge patterns: parent conrain child key")))))
              parent-map-pattern
              child-map-pattern))
 
-(defn- merge-request-patterns [parent child]
+(defn- merge-request-shapes [parent child]
   (let [parent-segments (get parent :segments [])
         child-segments (get child :segments [])
 
-        parent-map-pattern (dissoc parent :segments)
-        child-map-pattern (dissoc child :segments)
+        parent-map (dissoc parent :segments)
+        child-map (dissoc child :segments)
 
-        result-segments (merge-segments-patterns parent-segments child-segments)
-        result-map-pattern (merge-map-patterns parent-map-pattern child-map-pattern)]
+        result-segments (merge-segments-shapes parent-segments child-segments)
+        result-map-pattern (merge-map-shapes parent-map child-map)]
     (assoc result-map-pattern :segments result-segments)))
 
 (defn scope [s-name
@@ -57,7 +58,7 @@
      (-> route
          (update :scope conj s-name)
          (update :vars into vars)
-         (update :pattern #(merge-request-patterns pattern %))))
+         (update :pattern #(merge-request-shapes pattern %))))
    (flatten routes)))
 
 (defn- build-params-map [route]
