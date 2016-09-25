72af2f8abc3b5dd762a3d56de5a2d9d50b0dc330
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2016-09-25 15:56:47 +0400

    refactor low-lewel test

--- test/darkleaf/router/low_level_test.clj
+++ test/darkleaf/router/low_level_test.clj
@@ -1,5 +1,6 @@
 (ns darkleaf.router.low-level-test
   (:require [clojure.test :refer :all]
+            [clojure.template :refer [do-template]]
             [darkleaf.router.low-level :refer :all]))
 
 (def routes
@@ -30,51 +31,29 @@
     (route :not-found
            :handler identity))))
 
-(deftest test-matcher
-  (let [matcher (build-matcher routes)]
-    (are [request r-name r-scope r-params] (= (let [[matched-route route-params] (matcher request)]
-                                                [(:name matched-route) (:scope matched-route) route-params])
-                                              [r-name r-scope r-params])
-      {:segments [], :request-method :get}
-      :main-page [] {}
 
-      {:segments ["pages" "about"], :request-method :get}
-      :page [] {:slug "about"}
-
-      {:segments ["pages" "old-page"], :request-method :get}
-      :legacy-page [] {:slug "old-page"}
-
-      {:segments ["api" "pages"], :request-method :post, :headers {"token" "secret"}}
-      :create-page [:api] {:api-token "secret"}
-
-      {:segments ["api" "pages" "contacts"], :request-method :patch, :headers {"token" "secret"}}
-      :update-page [:api] {:slug "contacts", :api-token "secret"})))
-
-(deftest test-request-for
-  (let [request-for (build-reverse-matcher routes)]
-    (are [r-name r-scope r-params request] (= (request-for r-name r-scope r-params)
-                                              request)
-       :main-page [] {}
-       {:segments [], :request-method :get}
-
-       :page [] {:slug "about"}
-       {:segments ["pages" "about"], :request-method :get}
-
-       :legacy-page [] {:slug "old-page"}
-       {:segments ["pages" "old-page"], :request-method :get}
-
-       :create-page [:api] {:api-token "secret"}
-       {:segments ["api" "pages"], :request-method :post, :headers {"token" "secret"}}
-
-       :update-page [:api] {:slug "contacts", :api-token "secret"}
-       {:segments ["api" "pages" "contacts"], :request-method :patch, :headers {"token" "secret"}})))
-
-(deftest test-handler
+(deftest bidirectional-matching
   (let [matcher (build-matcher routes)
-        handler (build-handler matcher)
-        request {:uri "/pages/about", :request-method :get}
-        response (handler request)]
-    (is (= :page (get-in response [:matched-route :name])))
-    (is (= ["pages" "about"] (:segments response)))
-    (is (contains? response :matched-route))
-    (is (= {:slug "about"} (:route-params response)))))
+        reverse-matcher (build-reverse-matcher routes)]
+    (do-template [req-name req-scope req-params request]
+                 (testing req-name
+                   (is (= (reverse-matcher req-name req-scope req-params)
+                          request))
+                   (is (= (let [[matched-route route-params] (matcher request)]
+                            [(:name matched-route) (:scope matched-route) route-params]
+                            [req-name req-scope req-params]))))
+
+                 :main-page [] {}
+                 {:segments [], :request-method :get}
+
+                 :page [] {:slug "about"}
+                 {:segments ["pages" "about"], :request-method :get}
+
+                 :legacy-page [] {:slug "old-page"}
+                 {:segments ["pages" "old-page"], :request-method :get}
+
+                 :create-page [:api] {:api-token "secret"}
+                 {:segments ["api" "pages"], :request-method :post, :headers {"token" "secret"}}
+
+                 :update-page [:api] {:slug "contacts", :api-token "secret"}
+                 {:segments ["api" "pages" "contacts"], :request-method :patch, :headers {"token" "secret"}})))
