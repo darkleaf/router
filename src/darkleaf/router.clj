b341833048c8f105f485b8a2771c9a64f473c744
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2016-11-23 23:10:27 +0400

    save

--- src/darkleaf/router.clj
+++ src/darkleaf/router.clj
@@ -1,214 +1,214 @@
-(ns darkleaf.router
-  (:require [darkleaf.router.low-level :refer :all, :as ll])
-  (:require [clojure.string :refer [split join]]))
+;; (ns darkleaf.router
+;;   (:require [darkleaf.router.low-level :refer :all, :as ll])
+;;   (:require [clojure.string :refer [split join]]))
 
-(defn build-routes [& routes]
-  (combine-routes routes))
+;; (defn build-routes [& routes]
+;;   (combine-routes routes))
 
-(defn action
-  ([action-name handler]
-   (action :get action-name handler))
-  ([request-method action-name handler]
-   (route action-name
-          :pattern {:request-method request-method, ::ll/segments [(name action-name)]}
-          :template {:request-method request-method, ::ll/segments [(name action-name)]}
-          :handler handler)))
+;; (defn action
+;;   ([action-name handler]
+;;    (action :get action-name handler))
+;;   ([request-method action-name handler]
+;;    (route action-name
+;;           :pattern {:request-method request-method, ::ll/segments [(name action-name)]}
+;;           :template {:request-method request-method, ::ll/segments [(name action-name)]}
+;;           :handler handler)))
 
-(defn wildcard [request-method handler]
-  (route :wildcard
-         :vars '#{wildcard}
-         :pattern {:request-method request-method, ::ll/segments '[& wildcard]}
-         :template {:request-method request-method, ::ll/segments '[~@wildcard]}
-         :handler handler))
+;; (defn wildcard [request-method handler]
+;;   (route :wildcard
+;;          :vars '#{wildcard}
+;;          :pattern {:request-method request-method, ::ll/segments '[& wildcard]}
+;;          :template {:request-method request-method, ::ll/segments '[~@wildcard]}
+;;          :handler handler))
 
-(defn root [handler]
-  (route :root
-         :pattern {:request-method :get, ::ll/segments []}
-         :template {:request-method :get, ::ll/segments []}
-         :handler handler))
+;; (defn root [handler]
+;;   (route :root
+;;          :pattern {:request-method :get, ::ll/segments []}
+;;          :template {:request-method :get, ::ll/segments []}
+;;          :handler handler))
 
-(defn not-found [handler]
-  (route :not-found
-         :vars '#{requested-segments}
-         :pattern {::ll/segments '[& requested-segments]}
-         :template {::ll/segments '[~@requested-segments]}
-         :handler handler))
+;; (defn not-found [handler]
+;;   (route :not-found
+;;          :vars '#{requested-segments}
+;;          :pattern {::ll/segments '[& requested-segments]}
+;;          :template {::ll/segments '[~@requested-segments]}
+;;          :handler handler))
 
-(defn section [s-name & routes]
-  (scope s-name
-         {:pattern {::ll/segments [(name s-name)]}
-          :template {::ll/segments [(name s-name)]}}
-         routes))
+;; (defn section [s-name & routes]
+;;   (scope s-name
+;;          {:pattern {::ll/segments [(name s-name)]}
+;;           :template {::ll/segments [(name s-name)]}}
+;;          routes))
 
-(defn guard [g-name predicate & routes]
-  (let [name-symbol (-> g-name name symbol)]
-    (scope g-name
-           {:vars #{name-symbol}
-            :pattern {::ll/segments [(list name-symbol :guard predicate)]}
-            :template {::ll/segments [(list 'clojure.core/unquote name-symbol)]}}
-           routes)))
+;; (defn guard [g-name predicate & routes]
+;;   (let [name-symbol (-> g-name name symbol)]
+;;     (scope g-name
+;;            {:vars #{name-symbol}
+;;             :pattern {::ll/segments [(list name-symbol :guard predicate)]}
+;;             :template {::ll/segments [(list 'clojure.core/unquote name-symbol)]}}
+;;            routes)))
 
-(defn wrap-handler [middleware & routes]
-  (map
-   #(update % :handler middleware)
-   (flatten routes)))
+;; (defn wrap-handler [middleware & routes]
+;;   (map
+;;    #(update % :handler middleware)
+;;    (flatten routes)))
 
-(defn- collection-routes [rs-name controller additional-routes]
-  (wrap-handler
-   (get controller :middleware identity)
-   (scope rs-name
-          {:pattern {::ll/segments [(name rs-name)]}
-           :template {::ll/segments [(name rs-name)]}}
-          (cond-> []
-            (contains? controller :index)
-            (conj (route :index
-                         :pattern {:request-method :get, ::ll/segments []}
-                         :template {:request-method :get, ::ll/segments []}
-                         :handler (:index controller)))
+;; (defn- collection-routes [rs-name controller additional-routes]
+;;   (wrap-handler
+;;    (get controller :middleware identity)
+;;    (scope rs-name
+;;           {:pattern {::ll/segments [(name rs-name)]}
+;;            :template {::ll/segments [(name rs-name)]}}
+;;           (cond-> []
+;;             (contains? controller :index)
+;;             (conj (route :index
+;;                          :pattern {:request-method :get, ::ll/segments []}
+;;                          :template {:request-method :get, ::ll/segments []}
+;;                          :handler (:index controller)))
 
-            (contains? controller :new)
-            (conj (route :new
-                         :pattern {:request-method :get, ::ll/segments ["new"]}
-                         :template {:request-method :get, ::ll/segments ["new"]}
-                         :handler (:new controller)))
+;;             (contains? controller :new)
+;;             (conj (route :new
+;;                          :pattern {:request-method :get, ::ll/segments ["new"]}
+;;                          :template {:request-method :get, ::ll/segments ["new"]}
+;;                          :handler (:new controller)))
 
-            (contains? controller :create)
-            (conj (route :create
-                         :pattern {:request-method :post, ::ll/segments []}
-                         :template {:request-method :post, ::ll/segments []}
-                         :handler (:create controller))))
-          additional-routes)))
+;;             (contains? controller :create)
+;;             (conj (route :create
+;;                          :pattern {:request-method :post, ::ll/segments []}
+;;                          :template {:request-method :post, ::ll/segments []}
+;;                          :handler (:create controller))))
+;;           additional-routes)))
 
-(defn- member-routes [rs-name id-symbol controller additional-routes]
-  (wrap-handler
-   (comp (get controller :member-middleware identity)
-         (get controller :middleware identity))
-   (scope rs-name
-          {:vars #{id-symbol}
-           :pattern {::ll/segments [(name rs-name) id-symbol]}
-           :template {::ll/segments [(name rs-name) (list 'clojure.core/unquote id-symbol)]}}
-          (cond-> []
-            (contains? controller :show)
-            (conj (route :show
-                         :pattern {:request-method :get, ::ll/segments []}
-                         :template {:request-method :get, ::ll/segments []}
-                         :handler (:show controller)))
+;; (defn- member-routes [rs-name id-symbol controller additional-routes]
+;;   (wrap-handler
+;;    (comp (get controller :member-middleware identity)
+;;          (get controller :middleware identity))
+;;    (scope rs-name
+;;           {:vars #{id-symbol}
+;;            :pattern {::ll/segments [(name rs-name) id-symbol]}
+;;            :template {::ll/segments [(name rs-name) (list 'clojure.core/unquote id-symbol)]}}
+;;           (cond-> []
+;;             (contains? controller :show)
+;;             (conj (route :show
+;;                          :pattern {:request-method :get, ::ll/segments []}
+;;                          :template {:request-method :get, ::ll/segments []}
+;;                          :handler (:show controller)))
 
-            (contains? controller :edit)
-            (conj (route :edit
-                         :pattern {:request-method :get, ::ll/segments ["edit"]}
-                         :template {:request-method :get, ::ll/segments ["edit"]}
-                         :handler (:edit controller)))
+;;             (contains? controller :edit)
+;;             (conj (route :edit
+;;                          :pattern {:request-method :get, ::ll/segments ["edit"]}
+;;                          :template {:request-method :get, ::ll/segments ["edit"]}
+;;                          :handler (:edit controller)))
 
-            (contains? controller :update)
-            (conj (route :update
-                         :pattern {:request-method :patch, ::ll/segments []}
-                         :template {:request-method :patch, ::ll/segments []}
-                         :handler (:update controller)))
+;;             (contains? controller :update)
+;;             (conj (route :update
+;;                          :pattern {:request-method :patch, ::ll/segments []}
+;;                          :template {:request-method :patch, ::ll/segments []}
+;;                          :handler (:update controller)))
 
-            (contains? controller :destroy)
-            (conj (route :destroy
-                         :pattern {:request-method :delete, ::ll/segments []}
-                         :template {:request-method :delete, ::ll/segments []}
-                         :handler (:destroy controller))))
-          additional-routes)))
+;;             (contains? controller :destroy)
+;;             (conj (route :destroy
+;;                          :pattern {:request-method :delete, ::ll/segments []}
+;;                          :template {:request-method :delete, ::ll/segments []}
+;;                          :handler (:destroy controller))))
+;;           additional-routes)))
 
-(defn resources [rs-name id-symbol controller
-                 & {collection-rs :collection, member-rs :member
-                    :or {collection-rs [], member-rs []}}]
-  [(collection-routes rs-name controller collection-rs)
-   (member-routes rs-name id-symbol controller member-rs)])
+;; (defn resources [rs-name id-symbol controller
+;;                  & {collection-rs :collection, member-rs :member
+;;                     :or {collection-rs [], member-rs []}}]
+;;   [(collection-routes rs-name controller collection-rs)
+;;    (member-routes rs-name id-symbol controller member-rs)])
 
-(defn resource [r-name controller & inner-routes]
-  (wrap-handler
-   (get controller :middleware identity)
-   (scope r-name
-          {:pattern {::ll/segments [(name r-name)]}
-           :template {::ll/segments [(name r-name)]}}
-          (cond-> '()
-            (contains? controller :new)
-            (conj (route :new
-                         :pattern {:request-method :get, ::ll/segments ["new"]}
-                         :template {:request-method :get, ::ll/segments ["new"]}
-                         :handler (:new controller)))
+;; (defn resource [r-name controller & inner-routes]
+;;   (wrap-handler
+;;    (get controller :middleware identity)
+;;    (scope r-name
+;;           {:pattern {::ll/segments [(name r-name)]}
+;;            :template {::ll/segments [(name r-name)]}}
+;;           (cond-> '()
+;;             (contains? controller :new)
+;;             (conj (route :new
+;;                          :pattern {:request-method :get, ::ll/segments ["new"]}
+;;                          :template {:request-method :get, ::ll/segments ["new"]}
+;;                          :handler (:new controller)))
 
-            (contains? controller :create)
-            (conj (route :create
-                         :pattern {:request-method :post}
-                         :template {:request-method :post}
-                         :handler (:create controller)))
+;;             (contains? controller :create)
+;;             (conj (route :create
+;;                          :pattern {:request-method :post}
+;;                          :template {:request-method :post}
+;;                          :handler (:create controller)))
 
-            (contains? controller :show)
-            (conj (route :show
-                         :pattern '{:request-method :get}
-                         :template '{:request-method :get}
-                         :handler (:show controller)))
+;;             (contains? controller :show)
+;;             (conj (route :show
+;;                          :pattern '{:request-method :get}
+;;                          :template '{:request-method :get}
+;;                          :handler (:show controller)))
 
-            (contains? controller :edit)
-            (conj (route :edit
-                         :pattern '{:request-method :get, ::ll/segments ["edit"]}
-                         :template '{:request-method :get, ::ll/segments ["edit"]}
-                         :handler (:edit controller)))
+;;             (contains? controller :edit)
+;;             (conj (route :edit
+;;                          :pattern '{:request-method :get, ::ll/segments ["edit"]}
+;;                          :template '{:request-method :get, ::ll/segments ["edit"]}
+;;                          :handler (:edit controller)))
 
-            (contains? controller :update)
-            (conj (route :update
-                         :pattern '{:request-method :patch}
-                         :template '{:request-method :patch}
-                         :handler (:update controller)))
+;;             (contains? controller :update)
+;;             (conj (route :update
+;;                          :pattern '{:request-method :patch}
+;;                          :template '{:request-method :patch}
+;;                          :handler (:update controller)))
 
-            (contains? controller :destroy)
-            (conj (route :destroy
-                         :pattern '{:request-method :delete}
-                         :template '{:request-method :delete}
-                         :handler (:destroy controller))))
-          inner-routes)))
+;;             (contains? controller :destroy)
+;;             (conj (route :destroy
+;;                          :pattern '{:request-method :delete}
+;;                          :template '{:request-method :delete}
+;;                          :handler (:destroy controller))))
+;;           inner-routes)))
 
-;; ---------- builders ----------
+;; ;; ---------- builders ----------
 
-(defn- request-before-match [req]
-  (-> req
-      (assoc ::ll/segments (vec (rest (split (:uri req) #"/"))))))
+;; (defn- request-before-match [req]
+;;   (-> req
+;;       (assoc ::ll/segments (vec (rest (split (:uri req) #"/"))))))
 
-(defn- request-after-match [req & {:as stuff}]
-  (-> req
-      (dissoc ::ll/segments)
-      (merge stuff)))
+;; (defn- request-after-match [req & {:as stuff}]
+;;   (-> req
+;;       (dissoc ::ll/segments)
+;;       (merge stuff)))
 
-(defn matcher->handler [matcher]
-  (fn [original-req]
-    (let [req (request-before-match original-req)
-          [route params] (matcher req)
-          route-handler (:handler route)
-          result-req (request-after-match req
-                                          :route-params params
-                                          :matched-route route)]
-      (route-handler result-req))))
+;; (defn matcher->handler [matcher]
+;;   (fn [original-req]
+;;     (let [req (request-before-match original-req)
+;;           [route params] (matcher req)
+;;           route-handler (:handler route)
+;;           result-req (request-after-match req
+;;                                           :route-params params
+;;                                           :matched-route route)]
+;;       (route-handler result-req))))
 
-(defmacro build-handler [routes-var-name]
-  `(let [matcher# (build-matcher ~routes-var-name)]
-     (matcher->handler matcher#)))
+;; (defmacro build-handler [routes-var-name]
+;;   `(let [matcher# (build-matcher ~routes-var-name)]
+;;      (matcher->handler matcher#)))
 
-(defn- prepare-reverse-request [req]
-  (-> req
-      (assoc :uri (str "/" (join "/" (::ll/segments req))))
-      (dissoc ::ll/segments)))
+;; (defn- prepare-reverse-request [req]
+;;   (-> req
+;;       (assoc :uri (str "/" (join "/" (::ll/segments req))))
+;;       (dissoc ::ll/segments)))
 
-(defn matchers->request-for [matcher reverse-matcher]
-  (letfn [(request-for
-            ([r-name r-scope] (request-for r-name r-scope {}))
-            ([r-name r-scope r-params]
-             (let [raw-request (reverse-matcher r-name r-scope r-params)
-                   [matched-route _] (matcher raw-request)
-                   request (prepare-reverse-request raw-request)]
-               (if (and (= r-name (:name matched-route))
-                        (= r-scope (:scope matched-route)))
-                 request
-                 (throw (java.lang.IllegalArgumentException.
-                         (str "Can't match the same route for given params. "
-                              "Matched " (:name matched-route) " in scope " (:scope matched-route) ".")))))))]
-    request-for))
+;; (defn matchers->request-for [matcher reverse-matcher]
+;;   (letfn [(request-for
+;;             ([r-name r-scope] (request-for r-name r-scope {}))
+;;             ([r-name r-scope r-params]
+;;              (let [raw-request (reverse-matcher r-name r-scope r-params)
+;;                    [matched-route _] (matcher raw-request)
+;;                    request (prepare-reverse-request raw-request)]
+;;                (if (and (= r-name (:name matched-route))
+;;                         (= r-scope (:scope matched-route)))
+;;                  request
+;;                  (throw (java.lang.IllegalArgumentException.
+;;                          (str "Can't match the same route for given params. "
+;;                               "Matched " (:name matched-route) " in scope " (:scope matched-route) ".")))))))]
+;;     request-for))
 
-(defmacro build-request-for [routes-var-name]
-  `(let [matcher# (build-matcher ~routes-var-name)
-         reverse-matcher# (build-reverse-matcher ~routes-var-name)]
-     (matchers->request-for matcher# reverse-matcher#)))
+;; (defmacro build-request-for [routes-var-name]
+;;   `(let [matcher# (build-matcher ~routes-var-name)
+;;          reverse-matcher# (build-reverse-matcher ~routes-var-name)]
+;;      (matchers->request-for matcher# reverse-matcher#)))
--- src/darkleaf/router/low_level.clj
+++ src/darkleaf/router/low_level.clj
@@ -1,102 +1,56 @@
-(ns darkleaf.router.low-level
-  (:require [clojure.core.match :refer [match]]
-            [backtick :refer [syntax-quote-fn]]))
+(ns darkleaf.router.low-level)
 
-(defrecord Route [name
-                  vars pattern template
-                  handler scope])
+(defprotocol Processable
+  (process [this req]
+    "return response or :unprocessable"))
 
-(defn route
-  [name & {:keys [vars pattern template handler]
-           :or {vars #{}}}]
+(defrecord App [children]
+  Processable
+  (process [_ req]
+    (some #(process % req) children)))
+
+(defn app [& children]
+  (App. children))
+
+(defrecord Scope [name preprocessor children]
+  Processable
+  (process [_ req]))
+
+(defn scope [name {:keys [preprocessor]} & children]
+  (Scope. name preprocessor children))
+
+#_(defrecord Wrapper [preprocessor children]
+    Processable
+    (process [_ req]))
+
+(defrecord Action [name handler preprocessor]
+  Processable
+  (process [_ req]
+    (some-> req
+            preprocessor
+            handler)))
+
+(defn action
+  [name & {:keys [handler preprocessor]}]
   {:pre [(keyword? name)
-         (set? vars)
-         (map? pattern)
-         (map? template)
-         (ifn? handler)]}
-  (Route. name
-          vars pattern template
-          handler '()))
+         (ifn? handler)
+         (ifn? preprocessor)]}
+  (Action. name handler preprocessor))
 
-(defn- merge-segments-shapes [parent-segments child-segments]
-  (into parent-segments child-segments))
 
-(defn- merge-map-shapes [parent-map-pattern child-map-pattern]
-  (reduce-kv (fn [result k v]
-               (assoc result k
-                      (cond
-                        (and (map? (k result)) (map? v)) (merge-map-shapes (k result) v)
-                        (not (contains? result k)) v
-                        :else (throw (java.lang.IllegalArgumentException. "can't merge patterns: parent conrain child key")))))
-             parent-map-pattern
-             child-map-pattern))
-
-(defn- merge-request-shapes [parent child]
-  (let [parent-segments (get parent ::segments [])
-        child-segments (get child ::segments [])
-
-        parent-map (dissoc parent ::segments)
-        child-map (dissoc child ::segments)
-
-        result-segments (merge-segments-shapes parent-segments child-segments)
-        result-map-pattern (merge-map-shapes parent-map child-map)]
-    (assoc result-map-pattern ::segments result-segments)))
-
-(defn scope [s-name
-             {:keys [vars pattern template]
-              :or {vars #{}}}
-             & routes]
-  {:pre [(keyword? s-name)
-         (set? vars)
-         (map? pattern)
-         (map? template)]}
-  (map
-   (fn [route]
-     (-> route
-         (update :scope conj s-name)
-         (update :vars into vars)
-         (update :pattern #(merge-request-shapes pattern %))
-         (update :template #(merge-request-shapes template %))))
-   (flatten routes)))
-
-(defn- build-params-map [route]
-  (let [symbols (:vars route)
-        m-keys (vec (map keyword symbols))
-        m-vals (vec symbols)]
-    `(zipmap ~m-keys ~m-vals)))
-
-(defn- build-pattern-row [route]
-  [(:pattern route)])
-
-(defn- route->match-clause [idx route routes-var-name]
-  (let [pattern-row (build-pattern-row route)
-        action `[(nth ~routes-var-name ~idx) ~(build-params-map route)]]
-    (list pattern-row action)))
-
-(defmacro build-matcher [routes-var-name]
-  `(fn [req#]
-     (match
-      [req#]
-      ~@(let [routes (var-get (resolve routes-var-name))]
-          (apply concat
-                 (map-indexed
-                  (fn [idx route] (route->match-clause idx route routes-var-name))
-                  routes))))))
-
-(defmacro build-reverse-matcher [routes-var-name]
-  (let [r-params-symbol (gensym 'r-params)]
-    `(fn [r-name# r-scope# ~r-params-symbol]
-       (case [r-name# r-scope#]
-         ~@(let [routes (var-get (resolve routes-var-name))]
-             (mapcat
-              (fn [route]
-                (list
-                 [(:name route) (:scope route)]
-                 `(let [{:keys [~@(:vars route)]} ~r-params-symbol]
-                    ~(syntax-quote-fn (:template route)))))
-              routes))))))
-
-(defn combine-routes [& routes]
-  (-> routes
-      flatten
-      vec))
+;; (defn scope [s-name
+;;              {:keys [vars pattern template]
+;;               :or {vars #{}}}
+;;              & routes]
+;;   {:pre [(keyword? s-name)
+;;          (set? vars)
+;;          (map? pattern)
+;;          (map? template)]}
+;;   (map
+;;    (fn [route]
+;;      (-> route
+;;          (update :scope conj s-name)
+;;          (update :vars into vars)
+;;          (update :pattern #(merge-request-shapes pattern %))
+;;          (update :template #(merge-request-shapes template %))))
+;;    (flatten routes)))
