9b5b9ed5a43534c82f3792c95bc1f7891b282b2d
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2017-02-25 19:02:39 +0400

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject darkleaf/router "0.3.0-beta1"
+(defproject darkleaf/router "0.3.0"
   :description "Bidirectional Ring router. REST oriented."
   :url "https://github.com/darkleaf/router"
   :license {:name "Eclipse Public License"
